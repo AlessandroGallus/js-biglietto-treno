cf9278b9194c57204cdc9915e39037b8b6fa2ff2
AlessandroGallus <algal543@gmail.com>
2021-04-07 14:52:17 +0200

    Aggiunto arrotondamento al secondo decimale

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ parseFloat(costo);
 
 if (eta<18){
     costo = ((km*0.21)-((km*0.21)/100*20));
-    console.log("Il costo del biglietto è uguale a: " + costo);
+    console.log("Il costo del biglietto è uguale a: " + costo.toFixed(2));
 } else if (eta>65){
         costo = ((km*0.21)-((km*0.21)/100*40));
-        console.log("Il costo del biglietto è uguale a: " + costo);
+        console.log("Il costo del biglietto è uguale a: " + costo.toFixed(2));
     }else{
         costo=km*0.21;
-        console.log("Il costo del biglietto è uguale a: " + costo);
+        console.log("Il costo del biglietto è uguale a: " + costo.toFixed(2));
     }
